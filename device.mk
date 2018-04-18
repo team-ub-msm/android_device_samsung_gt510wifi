7983bcda280662187e5740ec1a731c2f99ce99ea
deadman96385 <seanhoyt963@gmail.com>
2018-04-18 11:57:27 -0500

    Add audio configs
    audio_platform_info was generated from stock audio hal blob
    Both mixer paths are stock
    
    Change-Id: Idf7503b0f89fdae62ebd34a4283a50d2fac03437
    (cherry picked from commit 3dcc3f6fcafe87e85dd76cbe6fca6de344962994)

--- device.mk
+++ device.mk
@@ -9,6 +9,11 @@ LOCAL_PATH := device/samsung/gt510wifi
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gt510wifi/overlay
 
