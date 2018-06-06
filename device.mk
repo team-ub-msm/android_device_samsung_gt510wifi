8afc9a1b6fecd9d860eadfd7daa525d7b7b2e09d
Virsys Electron <virsys.electron@gmail.com>
2018-06-06 17:40:32 -0500

    Add more audio configs
    Change-Id: I1a09f1e3fc191a43499cb2591e698ec8ce1d1fbf
    (cherry picked from commit 86da1a46fb4265f9cfec5ed627735234ba53b62c)

--- device.mk
+++ device.mk
@@ -9,10 +9,14 @@ LOCAL_PATH := device/samsung/gt510wifi
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
+# Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gt510wifi/overlay
